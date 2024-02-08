9c3c5688dcb6182acb95207a173c92a212f575e3
Ivan <vanya1254@yandex.ru>
2024-02-08 12:40:20 +0300

    after techer`s remarks and gpt help

--- index.js
+++ index.js
@@ -1,111 +1,80 @@
 const JSON_DATA = `{
-    "1": {
-      "name": "Йога",
-      "time": "10:00 - 11:00",
-      "maxParticipants": 15,
-      "currentParticipants": 8
-    },
-    "2": {
-      "name": "Пилатес",
-      "time": "11:30 - 12:30",
-      "maxParticipants": 10,
-      "currentParticipants": 5
-    },
-    "3": {
-      "name": "Кроссфит",
-      "time": "13:00 - 14:00",
-      "maxParticipants": 20,
-      "currentParticipants": 15
-    },
-    "4": {
-      "name": "Танцы",
-      "time": "14:30 - 15:30",
-      "maxParticipants": 12,
-      "currentParticipants": 12
-    },
-    "5": {
-        "name": "Бокс",
-        "time": "16:00 - 17:00",
-        "maxParticipants": 8,
-        "currentParticipants": 7
-    }
+  "1": {
+    "name": "Йога",
+    "time": "10:00 - 11:00",
+    "maxParticipants": 15,
+    "currentParticipants": 8
+  },
+  "2": {
+    "name": "Пилатес",
+    "time": "11:30 - 12:30",
+    "maxParticipants": 10,
+    "currentParticipants": 5
+  },
+  "3": {
+    "name": "Кроссфит",
+    "time": "13:00 - 14:00",
+    "maxParticipants": 20,
+    "currentParticipants": 15
+  },
+  "4": {
+    "name": "Танцы",
+    "time": "14:30 - 15:30",
+    "maxParticipants": 12,
+    "currentParticipants": 12
+  },
+  "5": {
+    "name": "Бокс",
+    "time": "16:00 - 17:00",
+    "maxParticipants": 8,
+    "currentParticipants": 7
+  }
 }`;
-
-const JSON_USER_DATA = `{
-    "1": false,
-    "2": false,
-    "3": false,
-    "4": false,
-    "5": false
-}`;
-
-`<tr> 
-<td>Йога</td>
-<td>10:00 - 11:00</td>
-<td>15</td> <td>8</td>
-<td class="button"><button>Записаться</button></td>
-<td class="button"><button>Отменить запись</button></td>
-</tr>`;
 
 const INITIAL_DATA = JSON.parse(JSON_DATA);
-const INITIAL_USER_DATA = JSON.parse(JSON_USER_DATA);
 const tbodyEl = document.getElementById("tbody");
 
-const createTrEl = (dataTr, id, wasRegistered) => {
-  let trEl = document.createElement("tr");
+const createTrElement = (data, id, wasRegistered) => {
+  const trEl = document.createElement("tr");
   trEl.dataset.id = id;
 
-  trEl.insertAdjacentHTML(
-    "beforeend",
-    `<tr> 
-        <td class="name">${dataTr.name}</td>
-        <td class="time">${dataTr.time}</td>
-        <td class="maxParticipants">${dataTr.maxParticipants}</td>
-        <td class="currentParticipants">${dataTr.currentParticipants}</td>
-        <td class="button"><button class="registerBtn" ${
-          wasRegistered || dataTr.maxParticipants === dataTr.currentParticipants
-            ? "disabled"
-            : ""
-        }>Записаться</button></td>
-        <td class="button"><button class="cancelBtn" ${
-          wasRegistered ? "" : "disabled"
-        }
-          >Отменить запись</button></td>
-    </tr>`
-  );
+  const { name, time, maxParticipants, currentParticipants } = data;
+
+  trEl.innerHTML = `<td class="name">${name}</td>
+                   <td class="time">${time}</td>
+                   <td class="maxParticipants">${maxParticipants}</td>
+                   <td class="currentParticipants">${currentParticipants}</td>
+                   <td class="button">
+                      <button class="registerBtn" ${
+                        wasRegistered || maxParticipants === currentParticipants
+                          ? "disabled"
+                          : ""
+                      }>
+                         Записаться
+                      </button>
+                   </td>
+                   <td class="button">
+                      <button class="cancelBtn" ${
+                        wasRegistered ? "" : "disabled"
+                      }>
+                         Отменить запись
+                      </button>
+                   </td>`;
 
   tbodyEl.appendChild(trEl);
 };
 
-const checkedRenderLessons = (dataUser) => {
-  const lessonsLocalStorage = localStorage.getItem("lessons");
-
-  if (lessonsLocalStorage) {
-    console.log(111);
-    let lessonsData = JSON.parse(lessonsLocalStorage);
-
-    for (const key in lessonsData) {
-      createTrEl(lessonsData[key], key, dataUser[key]);
-    }
-  } else {
-    console.log(222);
-    localStorage.setItem("lessons", JSON_DATA);
-
-    for (const key in INITIAL_DATA) {
-      createTrEl(INITIAL_DATA[key], key, dataUser[key]);
-    }
+const renderLessons = (lessonsData, userData) => {
+  for (const key in lessonsData) {
+    const wasRegistered = userData.hasOwnProperty(key) && userData[key];
+    createTrElement(lessonsData[key], key, wasRegistered);
   }
 };
 
-const updateTrEl = (target, action) => {
-  let trEl = target.closest("[data-id]");
-
-  let lessons = JSON.parse(localStorage.getItem("lessons"));
-  let userData = JSON.parse(localStorage.getItem("userData"));
-
-  let currentParticipants = trEl.querySelector(".currentParticipants");
-  let registerBtn = trEl.querySelector(".registerBtn");
-  let cancelBtn = trEl.querySelector(".cancelBtn");
+const updateLessonData = (lessons, userData, trEl, action) => {
+  const currentParticipants = trEl.querySelector(".currentParticipants");
+  const registerBtn = trEl.querySelector(".registerBtn");
+  const cancelBtn = trEl.querySelector(".cancelBtn");
 
   if (action) {
     lessons[trEl.dataset.id].currentParticipants += 1;
@@ -115,7 +84,7 @@ const updateTrEl = (target, action) => {
     cancelBtn.disabled = false;
   } else {
     lessons[trEl.dataset.id].currentParticipants -= 1;
-    userData[trEl.dataset.id] = false;
+    delete userData[trEl.dataset.id];
     currentParticipants.innerHTML = Number(currentParticipants.innerHTML) - 1;
     registerBtn.disabled = false;
     cancelBtn.disabled = true;
@@ -125,39 +94,38 @@ const updateTrEl = (target, action) => {
   localStorage.setItem("userData", JSON.stringify(userData));
 };
 
-const userDataLocalStorage = localStorage.getItem("userData");
+const initializeLocalStorage = () => {
+  if (!localStorage.getItem("lessons")) {
+    localStorage.setItem("lessons", JSON.stringify(INITIAL_DATA));
+  }
 
-if (userDataLocalStorage) {
-  console.log(333);
-
-  let userData = JSON.parse(userDataLocalStorage);
-  checkedRenderLessons(userData);
-} else {
-  console.log(444);
-
-  localStorage.setItem("userData", JSON_USER_DATA);
-  checkedRenderLessons(INITIAL_USER_DATA);
-}
-
-const registerBtnElClick = (target) => {
-  console.log(555);
-
-  const action = true;
-  updateTrEl(target, action);
+  if (!localStorage.getItem("userData")) {
+    localStorage.setItem("userData", JSON.stringify({}));
+  }
 };
 
-const cancelBtnElClick = (target) => {
-  console.log(target, 666);
+const handleButtonClick = (target, action) => {
+  const trEl = target.closest("[data-id]");
+  const lessons = JSON.parse(localStorage.getItem("lessons")) || INITIAL_DATA;
+  const userData = JSON.parse(localStorage.getItem("userData")) || {};
 
-  const action = false;
-  updateTrEl(target, action);
+  updateLessonData(lessons, userData, trEl, action);
 };
 
-tbodyEl.addEventListener("click", ({ target }) => {
+const handleTableClick = ({ target }) => {
   if (target.matches(".registerBtn")) {
-    registerBtnElClick(target);
+    handleButtonClick(target, true);
   }
   if (target.matches(".cancelBtn")) {
-    cancelBtnElClick(target);
+    handleButtonClick(target, false);
   }
-});
+};
+
+initializeLocalStorage();
+
+const userData = JSON.parse(localStorage.getItem("userData")) || {};
+const lessonsData = JSON.parse(localStorage.getItem("lessons")) || INITIAL_DATA;
+
+renderLessons(lessonsData, userData);
+
+tbodyEl.addEventListener("click", handleTableClick);
